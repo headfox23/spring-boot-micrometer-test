78c9a8d230ee818942b9ea6bdbfdbedfee1636a2
Hans Heisig <hans.heisig@swisscom.com>
2022-06-03 13:35:31 +0200

    reducing the example to the minimum

--- src/main/java/com/example/MicrometerTest.java
+++ src/main/java/com/example/MicrometerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @Slf4j
-@SpringBootApplication(exclude = RedisAutoConfiguration.class)
+@SpringBootApplication
 @EnableConfigurationProperties
 public class MicrometerTest {
     public static void main(String[] args) {
